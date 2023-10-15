2041a3b8ae350122ed5190e75b01a0f9e6171621
junemp <junemp@gmail.com>
2023-10-15 09:57:44 +0900

    수정 사항 적용

--- lib/app.dart
+++ lib/app.dart
@@ -63,6 +63,9 @@ class AppState extends State<App> with Nav, WidgetsBindingObserver {
         break;
       case AppLifecycleState.detached:
         break;
+      case AppLifecycleState.hidden:
+        // TODO: Handle this case.
+        break;
     }
     super.didChangeAppLifecycleState(state);
   }
